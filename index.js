6a4457fd0fe3c90c973dd50c80a8727a60227ca4
ankitsharma06 <asrover9810@gmail.com>
2024-03-09 21:14:51 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 let contacts=[  {   name:'Ankit (You)',
-<<<<<<< HEAD
 title:'message yourself',
 dp:'dps/Screenshot 2023-10-21 164615.png'},
-=======
-title:'massage yourself',
-dp:'Screenshot 2023-10-21 164615.png'},
->>>>>>> 6455dae127eae1265b8e0c6076cbee3e857e2f73
 {   name:'IT Placement 2025',
 title:'click for more info',
 dp:'dps/Screenshot 2023-08-03 151421.png'},
@@ -14,7 +9,7 @@ title:'click for more info',
 dp:'dps/Screenshot 2023-08-03 151605.png'},
 {   name:'Bhai&#10084;&#65039;',
 title:'',
-dp:'Screenshot 2023-10-21 155523.png'},
+dp:'dps/Screenshot 2023-10-21 155523.png'},
 {   name:'Talk about scholarship',
 title:'click for more info',
 dp:'dps/Screenshot 2023-08-03 151516.png'},
@@ -32,10 +27,7 @@ title:'',
 dp:'dps/Screenshot 2023-08-03 152711.png'},
 {   name:'Ayush cs',
 title:'',
-dp:'dps/Screenshot 2023-08-03 151421.png'},
-{   name:'&#128293;SYSTEM&#128293;',
-title:'click for more info',
-dp:'dps/Screenshot 2023-08-03 152215.png'},
+dp:'dps/Screenshot 2023-08-03 152729.png'},
 
 
 
